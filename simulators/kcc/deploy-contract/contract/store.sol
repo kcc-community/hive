7efe1733f2faefde059fc8ce71e9ced24b0d9b09
Lex <lex.wang@flsdex.com>
2023-02-27 16:35:46 +0800

    update contract

--- simulators/kcc/deploy-contract/contract/store.sol
+++ simulators/kcc/deploy-contract/contract/store.sol
@@ -6,7 +6,7 @@ contract Store {
     mapping(bytes32 => bytes32) public items;
 
     function setItem(bytes32 key, bytes32 value) external {
-        for (uint i = 1; i < 1000000; i++) {
+        for (uint i = 1; i < 100000; i++) {
             items[key] = value;
         }
         emit ItemSet(key, value);
